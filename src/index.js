18bcb66aa0637a26ddff6d4404c3343e37f59f69
DhyataGarg <dhyatagarg@gmail.com>
2022-09-04 01:21:35 +0530

    Display content

--- src/index.js
+++ src/index.js
@@ -5,34 +5,18 @@ import { CKEditor } from 'ckeditor4-react';
 const { useState } = React;
 
 const ConfigEvents = () => {
-	const [ events, setEvents ] = useState( [] );
+  const [content, setContent] = useState("This is a CKEditor 4 WYSIWYG editor instance created by ️⚛️ React.")
 
-	const logEvent = ( evt ) => {
-		evt.timestamp = new Intl.DateTimeFormat( 'en', {
-			hour12: false,
-			hour: '2-digit',
-			minute: '2-digit',
-			second: '2-digit'
-		} ).format( new Date() );
-
-		setEvents( events =>  [ evt, ...events ] );
-	}
-
-	const clearEvents = () => {
-		setEvents( [] );
-	}
+  const updateContent = (e) => {
+    setContent(e.editor.getData())
+  }
 
 	return (
 		<div>
-			<h2>WYSIWYG editor with custom event handlers and configuration</h2>
-			<p>
-				Editors created with the CKEditior 4 React component are highly customizable. It is possible to overwrite every configuration setting using the <code>config</code> property and passing an object containing the configuration to it.
-			</p>
-			<p>
-				Additionally, the CKEditor 4 WYSIWYG editor component for React allows you to bind any event handler using properties with names starting with <code>on</code>, followed by the name of the event with the first letter capitalized. The following example shows how to bind several common CKEditor 4 events and apply custom toolbar configuration.
-			</p>
 			<CKEditor
-				initData="This is a CKEditor 4 WYSIWYG editor instance created by ️⚛️ React."
+      initData={content}
+        onChange={updateContent}
+        content={content}
 				config={{
 					toolbar: [
 						[ 'Source' ],
@@ -48,42 +32,15 @@ const ConfigEvents = () => {
 					cloudServices_tokenUrl:
 						'https://33333.cke-cs.com/token/dev/ijrDsqFix838Gh3wGO3F77FSW94BwcLXprJ4APSp3XQ26xsUHTi0jcb1hoBt'
 				}}
-				onFocus={logEvent}
-				onBlur={logEvent}
-				onChange={logEvent}
-				onSelectionChange={logEvent}
 			/>
-			<h3>Events Log</h3>
-			<small>To check additional details about every event, consult the console in the browser developer tools.</small>
-			<EventLog stream={events} />
-			<button onClick={clearEvents}>Clear events log</button>
+      <br/>
+      <h1>
+        Content
+      </h1>
+    <div
+      dangerouslySetInnerHTML={{__html: content}}
+    />
 		</div>
-	);
-}
-
-const EventLog = ( { stream } ) => {
-	return (
-		<div className="event-log">
-			<ul className="event-log__events">
-				{
-					stream.map( event => {
-						return (
-							<li className="event-log__event">
-								<Event data={event} />
-							</li>
-						)
-					} )
-				}
-			</ul>
-		</div>
-	);
-}
-
-const Event = ( { data: { name, timestamp } } ) => {
-	return (
-		<>
-			{timestamp} – {name}
-		</>
 	);
 }
 
